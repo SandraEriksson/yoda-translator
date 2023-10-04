ea564502ceabd8b0fa6db79895e581dbd17b38f8
SandraEriksson <SandraErikss0n@hotmail.com>
2023-10-04 14:29:14 +0200

    comments

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 
+//import necessary libraries
 import React, { useState } from 'react';
 import './App.css';
 import Translate from './Translate';
 import { createContext } from 'react';
 import ReactSwitch from 'react-switch';
 
-
+//Create a context for managing the theme
 export const ThemeContext = createContext("null");
 
 function App() {
+  //Initialize the theme state with "light" as default
   const [theme, setTheme] = useState("light");
 
+  //function to toggle between "light" and "dark" theme
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"))
-  }  
-    return(
-    <ThemeContext.Provider value={{ theme, toggleTheme }}> 
-    <div className="App">
-      <header className="App-header" id={theme}>
-        <div className="box">
-        <div className="switch">
-        <h4> {theme === "light" ? "Light Mode": "Dark Mode"}</h4>
-        <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
-        </div>
-        <Translate />
-        </div>
-      </header>
-    </div>
+  }
+  return (
+    //Provide the theme and toggleTheme funciton to child component via context
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App">
+        <header className="App-header" id={theme}>
+          <div className="box">
+            <div className="switch">
+              <h4> {theme === "light" ? "Light Mode" : "Dark Mode"}</h4>
+              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+            </div>
+            <Translate />
+          </div>
+        </header>
+      </div>
     </ThemeContext.Provider>
   );
 }
--- src/Translate.js
+++ src/Translate.js
@@ -2,49 +2,54 @@ import React, { useState } from 'react';
 import './App.css';
 
 function Translate() {
+    //Define state varibles for input text and Yoda's translated text
     let [text, setText] = useState("");
     let [yodaText, setYodaText] = useState("");
 
+    //Define the click event handler
     const onClick = () => {
+        //Sending a POST request to the Yoda translation API
         fetch("/translate/yoda.json", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ text }),
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ text }),
         })
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("Network response was not ok");
-            }
-            return response.json();
-          })
-          .then((data) => {
-            const { translated } = data.contents;
-            setYodaText(translated);
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-          });
-      };
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                //Extract the translated text from the API response
+                const { translated } = data.contents;
+                //Update the Yoda text state with the translation
+                setYodaText(translated);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
 
-
+    //Return the JSX for the Translate component
     return (
-            <>
-                <h1>Yoda Translation</h1>
-                <div className="inputBox">
-                    <input type="text" value={text} onChange={e => setText(e.target.value)} placeholder="Type something in english.." />
-                    <button onClick={onClick}>Translate</button>
+        <>
+            <h1>Yoda Translation</h1>
+            <div className="inputBox">
+                <input type="text" value={text} onChange={e => setText(e.target.value)} placeholder="Type something in english.." />
+                <button onClick={onClick}>Translate</button>
+            </div>
+            <main>
+                <div className="translate-box">
+                    <p>{yodaText}</p>
                 </div>
-                <main>
-                    <div className="translate-box">
-                        <p>{yodaText}</p>
-                    </div>
-                    <div className="yoda-img">
-                        <img alt="Yoda"></img>
-                    </div>
-                </main>
-            </>
+                <div className="yoda-img">
+                    <img alt="Yoda"></img>
+                </div>
+            </main>
+        </>
     );
 }
 
